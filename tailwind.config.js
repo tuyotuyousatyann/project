ee3ba2f3b7b2c0620b737bf1fb11774876de049b
tuyotuyousatyann <kimiomou0318@gmail.com>
2024-04-27 14:53:12 +0900

    masterKey生成

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  plugins: [require("daisyui")],
+}
+
